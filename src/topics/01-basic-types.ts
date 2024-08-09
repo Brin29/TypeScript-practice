53454f0b1e3d0fc9975fa7c790a1b3c5323bdf3c
Brin29 <breinerstiveparracortes50@gmail.com>
2024-08-09 17:01:29 -0500

    primer commit

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,36 @@
+// numbers, string, boolean, Date ,any
+
+//#region NUMBER
+
+console.log("**********************");
+console.log("******* NUMBER *******");
+console.log("**********************");  
+
+// TypeScript ya refiere el tipo de dato
+let age = 15;
+
+// No se pueden reasignar el tipo de dato
+// age = "";
+
+console.log(age);
+
+// Declaracion del tipo de dato
+let weight: number;
+
+// Convertir el dato parseInt, parseFloat, Number
+
+const ageText = "19";
+parseInt(ageText);
+
+console.log(ageText);
+
+
+//#endregion
+
+//#region STRING
+
+
+//#endregion
+
+
+export {};
